8d8f0cfd73f08f82438b415989ea3150436d889d
cw <chiewwei@greywolf.sg>
2025-03-10 18:00:51 +0800

    Update common.go

--- util/common.go
+++ util/common.go
@@ -1,3 +1,13 @@
 package util
 
+import "encoding/json"
+
 // commonly used function
+
+func Recast(a, b interface{}) error {
+	js, err := json.Marshal(a)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(js, b)
+}
